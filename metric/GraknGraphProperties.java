a01bb8cdd7028f37400b14ac02b2708222241161
Joshua Send <joshua@Joshuas-MacBook-Pro.local>
2018-12-19 16:19:01 +0000

    Implement degree distribution using Grakn

--- metric/GraknGraphProperties.java
+++ metric/GraknGraphProperties.java
@@ -1,14 +1,36 @@
 package grakn.benchmark.metric;
 
+import grakn.core.GraknTxType;
+import grakn.core.Keyspace;
+import grakn.core.client.Grakn;
+import grakn.core.graql.Syntax;
+import grakn.core.graql.answer.ConceptSetMeasure;
+import grakn.core.graql.answer.Value;
+import grakn.core.util.SimpleURI;
 import org.apache.commons.math3.util.Pair;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class GraknGraphProperties implements GraphProperties {
 
-    public GraknGraphProperties(String uri, String keyspace) {
+    Grakn client;
+    Grakn.Session session;
+    Grakn.Transaction tx;
 
+    public GraknGraphProperties(String uri, String keyspace) {
+        this.client = new Grakn(new SimpleURI(uri));
+        this.session = client.session(Keyspace.of(keyspace));
+    }
+
+    private Grakn.Transaction getTx(boolean useWriteTx) {
+        if (useWriteTx) {
+            return session.transaction(GraknTxType.WRITE);
+        } else {
+            return session.transaction(GraknTxType.READ);
+        }
     }
 
 
@@ -29,7 +51,28 @@ public class GraknGraphProperties implements GraphProperties {
 
     @Override
     public Stream<Long> vertexDegree() {
-        return null;
+
+        // TODO `compute centrality using degree` doesn't return 0 for disconnected entities
+
+        // TODO do we need inference enabled here?
+        Grakn.Transaction tx = getTx(false);
+
+        List<ConceptSetMeasure> answerMap = tx.graql().compute(Syntax.Compute.Method.CENTRALITY).of("vertex").execute();
+
+        // repeatedly emit the degree
+        Stream<Long> nonzeroDegrees = answerMap.stream().map(
+                // take each degree and the number of vertices associated with it, then emit the degree that many times
+                conceptSetMeasure -> IntStream.range(0, conceptSetMeasure.set().size()).
+                        mapToLong(i -> conceptSetMeasure.measurement().longValue())
+        ).flatMap(e -> e.boxed()); // convert LongStream to Stream<Long>
+
+        long numNonzeroDegrees = answerMap.stream().map(conceptSetMeasure -> conceptSetMeasure.set().size()).reduce((a,b) -> a+b).orElse(0);
+
+        // count total number of vertices to see how many have degree == 0
+        List<Value> conceptCounts = tx.graql().compute(Syntax.Compute.Method.COUNT).in("vertex").execute();
+        long totalVertices = conceptCounts.get(0).asValue().number().longValue();
+
+        return Stream.concat(nonzeroDegrees, IntStream.range(0, (int)(totalVertices-numNonzeroDegrees)).map(i->0).asLongStream().boxed());
     }
 
     @Override
