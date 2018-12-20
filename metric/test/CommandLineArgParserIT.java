56a199353cddf46675fa0c543c440ad51f0568ca
Joshua Send <joshua@Joshuas-MacBook-Pro.local>
2018-12-20 14:13:06 +0000

    remove unused ITs

--- metric/test/CommandLineArgParserIT.java
+++
@@ -1,9 +0,0 @@
-package grakn.benchmark.metric.test;
-
-import org.junit.Test;
-
-public class CommandLineArgParserIT {
-
-    // TODO pass in --flags and expect an object that has queryable values
-
-}
