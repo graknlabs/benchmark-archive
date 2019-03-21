2169b62901a7b0d704a5b885af323ea064a49597
Joshua Send <joshuasend1@gmail.com>
2019-03-21 12:01:43 +0000

    tmp

--- generator/test/provider/CentralConceptProviderTest.java
+++ generator/test/provider/CentralConceptProviderTest.java
@@ -5,6 +5,7 @@ import grakn.core.concept.ConceptId;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
