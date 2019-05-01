d11e70d1c3faf7c600c6396eecb531c7330107f7
sorsaffari <soroush@grakn.ai>
2019-05-01 19:17:17 +0100

    add queries action bar in the inspect page

--- service/dashboard/src/views/inspect/TabularView/GraphTab.vue
+++ service/dashboard/src/views/inspect/TabularView/GraphTab.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
-    <el-row
-      type="flex"
-      justify="end"
-    >
-      <scale-selector
-        title="Scale"
-        :items="scales.map(scale => ({ text: scale, value: scale }))"
-        :defaultItem="{ text: selectedScale, value: selectedScale }"
-        @item-selected="this.selectedScale = scale;"
-      />
+    <el-row type="flex" justify="end" class="queries-action-bar">
+      <div class="action-item">
+        <scale-selector
+          title="Scale"
+          :items="scales.map(scale => ({ text: scale, value: scale }))"
+          :defaultItem="{ text: selectedScale, value: selectedScale }"
+          @item-selected="this.selectedScale = scale;"
+        />
+      </div>
     </el-row>
     <queries-table
       v-for="scale in scales"
@@ -22,13 +21,11 @@
   </div>
 </template>
 <script>
-
-import QueriesTable from './QueriesTable.vue';
-import ScaleSelector from '@/components/Selector.vue';
-
+import QueriesTable from "./QueriesTable.vue";
+import ScaleSelector from "@/components/Selector.vue";
 
 export default {
-  name: 'GraphTab',
+  name: "GraphTab",
 
   components: { ScaleSelector, QueriesTable },
 
@@ -46,22 +43,44 @@ export default {
     preSelectedScale: {
       type: Number,
       required: false
-    },
+    }
   },
 
   data() {
     return {
       scales: [],
-      selectedScale: null,
+      selectedScale: null
     };
   },
 
   created() {
     this.scales = [
-      ...new Set(this.spans.map(span => span.tags.graphScale)),
+      ...new Set(this.spans.map(span => span.tags.graphScale))
     ].sort((a, b) => a - b);
     this.selectedScale = this.preSelectedScale || this.scales[0];
-  },
+  }
 };
-
 </script>
+
+<style scoped lang="scss">
+@import "./src/assets/css/variables.scss";
+
+.queries-action-bar {
+  height: 39px;
+
+  border-bottom: 1px solid $color-light-border;
+
+  align-items: center;
+  display: flex;
+  justify-content: flex-end;
+  margin-top: -$margin-default;
+  margin-right: -$margin-default;
+  margin-bottom: $margin-default;
+  margin-left: -$margin-default;
+
+  .action-item {
+    padding-right: $padding-default;
+  }
+}
+</style>
+
