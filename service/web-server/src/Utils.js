d96505ddd6945c228281fa3413686851bc4fec85
sorsaffari <soroush@grakn.ai>
2019-06-28 11:51:36 +0100

    setup Github oauth

--- service/web-server/src/Utils.js
+++ service/web-server/src/Utils.js
@@ -1,3 +1,4 @@
+const Octokit = require('@octokit/rest');
 const { spawn } = require('child_process');
 
 function displayStream(stream){
@@ -5,20 +6,58 @@ function displayStream(stream){
         stream.stdout.on('data', (data) => {
             console.log(`${data}`);
         });
-          
+
         stream.stderr.on('data', (data) => {
             process.stdout.write(`${data}`);
         });
-          
+
         stream.on('close', (code) => {
             if(code !== 0){
                 console.error(`Script terminated with code ${code}`);
             }
         });
     })
-   
+
 }
-    
+const ORG_TOKEN = process.env.GITHUB_ORG_TOKEN;
+const orgOctokit = new Octokit({ auth: ORG_TOKEN });
+const CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+const getGithubUserAccessToken = async (oauthCode) => {
+    const accessTokenResp = await orgOctokit.request('POST https://github.com/login/oauth/access_token', {
+        headers: { Accept: "application/json" },
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        code: oauthCode
+    });
+    return accessTokenResp.data.access_token;
+}
+
+const getGithubUserId = async (accessToken) => {
+    const userOctokit = new Octokit({ auth: accessToken });
+    try {
+        const userResp = await userOctokit.request('Get https://api.github.com/user', {
+            headers: { Accept: "application/json" },
+            access_token: accessToken,
+        });
+        return userResp.data.id
+    } catch (err) {
+        throw err;
+    }
+}
+
+const isUserGraknLabsMember = async (userId) => {
+    try {
+        const membersResp = await orgOctokit.orgs.listMembers({ org: 'graknlabs' });
+        const members = membersResp.data;
+        const isUserMember = members.find((member) => member.id === userId);
+        return isUserMember ? true : false;
+    } catch (err) {
+        throw(err)
+    }
+}
+
 module.exports = {
     parseMergedPR(req){
         return {
@@ -41,14 +80,17 @@ module.exports = {
             executionInitialisedAt: new Date().toISOString(),
             status: 'INITIALISING',
             vmName: 'benchmark-executor-'+ req.body.commit.trim()
-        } 
+        }
     },
     startBenchmarking(scriptPath, execution){
         const ls = spawn('bash', [scriptPath, execution.repoUrl, execution.id, execution.commit, execution.vmName])
-        displayStream(ls);  
+        displayStream(ls);
     },
     deleteInstance(scriptPath, vmName){
         const ls = spawn('bash', [scriptPath, vmName])
-        displayStream(ls);  
-    }
+        displayStream(ls);
+    },
+    getGithubUserAccessToken,
+    getGithubUserId,
+    isUserGraknLabsMember
 }
