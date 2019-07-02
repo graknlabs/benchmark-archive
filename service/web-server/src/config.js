825fe7356672089440f0cf327fb663b9cde37eb7
sorsaffari <soroush@grakn.ai>
2019-07-02 15:35:01 +0100

    remove merge conflict lines

--- service/web-server/src/config.js
+++ service/web-server/src/config.js
@@ -5,13 +5,9 @@ config.web = {};
 
 config.es.host = '0.0.0.0';
 config.es.port = 9200;
-<<<<<<< Updated upstream
-config.web.port = process.env.WEB_PORT || 4567;
-=======
 config.web.port = {
     http: 80,
     https: 443
 }
->>>>>>> Stashed changes
 
 module.exports = config;
