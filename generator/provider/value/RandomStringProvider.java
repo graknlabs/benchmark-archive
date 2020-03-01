319fc0179e5ca279a035f2d7bcee678ee7f641aa
Max Vorobev <vmax0770@gmail.com>
2020-03-01 20:28:09 +0300

    Update copyright headers to 2020

--- generator/provider/value/RandomStringProvider.java
+++ generator/provider/value/RandomStringProvider.java
@@ -1,19 +1,19 @@
 /*
- *  GRAKN.AI - THE KNOWLEDGE GRAPH
- *  Copyright (C) 2019 Grakn Labs Ltd
+ * Copyright (C) 2020 Grakn Labs
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as
- *  published by the Free Software Foundation, either version 3 of the
- *  License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
  */
 
 package grakn.benchmark.generator.provider.value;
@@ -46,8 +46,9 @@ public class RandomStringProvider implements ValueProvider<String> {
      * Generate a random string.
      */
     public String next() {
-        for (int idx = 0; idx < buf.length; ++idx)
+        for (int idx = 0; idx < buf.length; ++idx) {
             buf[idx] = symbols[random.nextInt(symbols.length)];
+        }
         return new String(buf);
     }
 
@@ -55,8 +56,9 @@ public class RandomStringProvider implements ValueProvider<String> {
      * Create an alphanumeric string generator.
      */
     public RandomStringProvider(Random random, int stringLength) {
-        if (stringLength < 1)
+        if (stringLength < 1) {
             throw new IllegalArgumentException("Require randomly generated strings to have length > 1");
+        }
         this.random = Objects.requireNonNull(random);
         this.symbols = ALPHANUM.toCharArray();
         this.buf = new char[stringLength];
